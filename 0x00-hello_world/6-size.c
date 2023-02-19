f4452861c55b196b1da4747f93bdd46a354ea64b
Christian Ahammefule <chrisaham96@gmail.com>
2023-02-19 11:42:19 -0800

    SIZE OF VARIOUS TYPES

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,9 @@
  */
 int main(void)
 {
-	printf("size of char: %i byte(s)",sizeof(char));
-	printf("size of int: %i bytes(s),"sizeof(int));
-	printf("size of float: %i bytes(s)",sizeof(float));
-	printf("size of long long int: %i byte(s)",sizeof(long long int));
-	printf("size of long int: %i byte(s)",sizeof(long int));
+	printf("size of char: %i byte(s)\n",sizeof(char));
+	printf("size of int: %i bytes(s)\n,"sizeof(int));
+	printf("size of float: %i bytes(s)\n",sizeof(float));
+	printf("size of long long int: %i byte(s)\n",sizeof(long long int));
+	printf("size of long int: %i byte(s)\n",sizeof(long int));
 }
