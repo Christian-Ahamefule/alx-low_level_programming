8c2d21d7004c4ec16a943b8bd6b7bf809f6d3837
Christian Ahammefule <chrisaham96@gmail.com>
2023-02-19 12:31:11 -0800

    Resolve error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+
 /**
- * main - entry point
- * Return : 0
+ * main - print the string
+ *
+ * Return: 0
  */
 int main(void)
 {
-	char C;
-	int t;
-	float d;
-	long long y;
-	long z;
-	printf("size of char: %ld byte(s)\n",sizeof(C));
-	printf("size of int: %ld bytes(s)\n,"sizeof(t));
-	printf("size of float: %ld bytes(s)\n",sizeof(d));
-	printf("size of long long: %ld byte(s)\n",sizeof(y));
-	printf("size of long: %ld byte(s)\n",sizeof(z));
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
