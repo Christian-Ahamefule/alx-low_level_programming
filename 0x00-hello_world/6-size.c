70a33f661b273123fe9ad286c2c006a20ac0919f
Christian Ahammefule <chrisaham96@gmail.com>
2023-02-19 12:28:32 -0800

    RESOLVE COMPILING ERROR

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,12 @@ int main(void)
 	char C;
 	int t;
 	float d;
-	long long int y;
-	long int z;
+	long long y;
+	long z;
 	printf("size of char: %ld byte(s)\n",sizeof(C));
 	printf("size of int: %ld bytes(s)\n,"sizeof(t));
 	printf("size of float: %ld bytes(s)\n",sizeof(d));
-	printf("size of long long int: %ld byte(s)\n",sizeof(y));
-	printf("size of long int: %ld byte(s)\n",sizeof(z));
+	printf("size of long long: %ld byte(s)\n",sizeof(y));
+	printf("size of long: %ld byte(s)\n",sizeof(z));
 	return (0);
 }
