33ea15e6baab328cc6eec2ad6324bdf411ffdeb0
Christian Ahammefule <chrisaham96@gmail.com>
2023-02-19 11:19:32 -0800

    PRINT WITH PRINTF FUNCTION

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ * Return = 0 Always (sucess)
+ */
+int main(void)
+{
+	prinf(%S,"with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
