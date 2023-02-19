1fce5e33ef2dcd3c5b25a6b1f5e65ab4c44a0083
Christian Ahammefule <chrisaham96@gmail.com>
2023-02-19 08:35:45 -0800

    RESOLVE BETTY CODING STYLE AND DOCUMENTATION

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point 
  *
  * return - always 0 (success)
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
