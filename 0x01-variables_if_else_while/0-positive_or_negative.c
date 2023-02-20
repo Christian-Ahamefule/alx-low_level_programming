17f9d7f5df8d4ce0b5b409f8fcdec219b226b675
Christian Ahammefule <chrisaham96@gmail.com>
2023-02-20 14:01:58 -0800

    RESOLVE COMPILATION ERROR

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdh.io> /* included a standard header */
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -15,9 +16,9 @@ int main(void)
 	if (n>0) {
 		printf("is positive\n");
 	} else if (n<0) {
-		printf("is negative");
+		printf("is negative\n");
 	} else {
-		printf("is zero")
+		printf("is zero\n");
 	}
 	return (0);
 }
