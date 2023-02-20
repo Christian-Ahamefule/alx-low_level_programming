7ee1fc634c4d586792d12c2816c8085c6a0b887f
Christian Ahammefule <chrisaham96@gmail.com>
2023-02-20 14:21:32 -0800

    RESOLVE OUTPUT ERROR AND BETTY STYLE

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,8 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
+/*main = Entry point */
 int main(void)
 {
 	int n;
@@ -12,12 +14,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	if (n>0) {
-		printf("%d\n is positive\n",n);
-	} else if (n<0) {
-		printf("%d is negative\n",n);
-	} else {
-		printf("%d is zero\n",n);
+	if (n > 0) {
+		printf("%d is positive\n", n);
+	} else if (n < 0) {
+		printf("%d is negative\n", n);
+	} else
+	{
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
